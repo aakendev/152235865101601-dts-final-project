2f8f0bdb21087b2cec6f946c811a9d28b0aed1c0
Aldy Rizky Putra <aakendev@gmail.com>
2022-07-28 23:06:58 +0700

    Adding DropdownMenu Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import kranjanku from './assets/images/kranjanku.png';
-import Button from './components/Button';
-import JumboTitle from './components/JumboTitle';
+import DropdownMenu from './components/DropdownMenu';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,15 +20,26 @@ const App = () => {
     }
   }, []);
   
+  const menuItem = [
+    {
+      title: "Delete",
+      iconActive: "heroicons-solid:trash",
+      iconInactive: "heroicons-outline:trash",
+    },
+    {
+      title: "Edit",
+      iconActive: "heroicons-solid:pencil",
+      iconInactive: "heroicons-outline:pencil",
+    },
+  ];
+  
   return isLoading ?
     <div className="w-full min-h-screen flex flex-col items-center justify-center">
       <p className="text-xs font-black animate-bounce">KranjanKu on Loading...</p>
       <img className="w-3/4 animate-pulse" src={kranjanku} alt="Logo KranjanKu" />
     </div> :
     <div>
-      <JumboTitle title="Jumbo Title" txt="text-fire-600" m="m-2" />
-      <JumboTitle title="Jumbo Title" txt="text-water-100" m="m-2" />
-      <Button title="Button Fire" bg="bg-fire-50 hover:bg-fire-50" txt="text-white" href="#" m="m-2" />
+      <DropdownMenu title="Menu Dropdown" btnBg="bg-fire-400 hover:bg-fire-500" menuBg="bg-white" menuBgTxt="bg-fire-500 text-white font-medium" menuTxtInactive="text-fire-900" btnTxt="text-md font-medium text-white" padding="px-3 py-3" iconMargin="mr-2" menuItem={menuItem} />
     </div>
 }
 
